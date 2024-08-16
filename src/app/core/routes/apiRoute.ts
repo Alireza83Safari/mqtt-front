80067c77d9ffd77958738e0346a428a0bda73ae5
Alireza <alireza83safarii@gmail.com>
2024-08-16 11:08:14 +0330

    feat: add routes

---
+++ src/app/core/routes/apiRoute.ts
@@ -0,0 +1,5 @@
+export class ApiRoutes {
+  static LOGIN = 'auth/login';
+  static REGISTER = 'auth/register';
+  static LOGOUT = 'auth/logout';
+}
---
+++ src/app/core/routes/pageRoutes.ts
@@ -0,0 +1,11 @@
+export class PageRoutes {
+  static LOGIN = 'login';
+  static REGISTER = 'register';
+  static BATTERY = 'battery';
+  static PROFILE = 'profile';
+}
+
+export class PrivateRoutes {
+  static ROLE = 'profile';
+  static USER = 'user';
+}
