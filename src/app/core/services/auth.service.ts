78f497e9fca9d05c0683a259b46bb743ac1a1b67
Alireza <alireza83safarii@gmail.com>
2024-08-17 11:17:52 +0330

    feat: add login page

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment.development';
 import { Login, Register } from '../models/auth';
 import { Observable } from 'rxjs';
 import { ApiRoutes } from '../routes/apiRoute';
@@ -10,12 +9,9 @@ import { ApiRoutes } from '../routes/apiRoute';
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
-
-  private apiUrl = environment.apiUrl;
+  private apiUrl = 'api/v2/';
 
   login(data: Login): Observable<any> {
-    console.log(ApiRoutes?.LOGIN);
-
     return this.http.post(this.apiUrl + ApiRoutes.LOGIN, data);
   }
 
--- src/app/features/auth/page/login/login.component.ts
+++ src/app/features/auth/page/login/login.component.ts
@@ -1,59 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from "@angular/forms";
-import { ErrorStateMatcher } from "@angular/material/core";
-import { AuthService } from "src/app/core/services/auth.service";
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  serverErrors!: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username: ["", [Validators.required, Validators.minLength(3)]],
-      password: ["", [Validators.required, Validators.minLength(8)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
-
-  matcher = new MyErrorStateMatcher();
 
   submitLogin() {
     if (this.loginForm.valid) {
       this.authService.login(this.loginForm.value).subscribe(
         (response) => {
-          console.log("Login successful", response);
+          this.toastrService.success(
+            `hi ${response?.result?.username} welcome to unicorn`
+          );
+          this.router.navigate(['/']);
         },
         (error) => {
-          console.error("Login failed", error);
+          this.serverErrors = error?.error?.message;
         }
       );
-    } else {
-      console.log("Form is invalid");
     }
   }
 }
