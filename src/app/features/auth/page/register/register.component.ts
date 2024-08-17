f46816c180e2cd617a0a4ad593df23e380e82f54
Alireza <alireza83safarii@gmail.com>
2024-08-17 12:07:42 +0330

    feat: add register page

--- src/app/features/auth/page/register/register.component.ts
+++ src/app/features/auth/page/register/register.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Register } from 'src/app/core/models/auth';
+import { passwordMatchValidator } from 'src/app/core/utils/passwordMatchValidator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  serverErrors!: { message: Register; errors: Register };
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]],
+        firstName: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['', [Validators.required, Validators.minLength(3)]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+      },
+      { validators: passwordMatchValidator() }
+    );
+  }
+
+  submitRegister() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe(
+        (response) => {
+          if (response) {
+            this.toastrService.success('Registration successful!');
+            this.router.navigate(['/']);
+          }
+        },
+        (error) => {
+          if (error?.error?.message) {
+            this.toastrService.error(error?.error?.message);
+          }
+          this.serverErrors = error.error;
+        }
+      );
+    }
+  }
 }
