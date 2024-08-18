4ef3379953a0525cca6b6f2c7d614e72d9a8e2e7
Alireza <alireza83safarii@gmail.com>
2024-08-18 23:39:29 +0330

    feat: add components on declarations

--- src/app/features/portal/portal.module.ts
+++ src/app/features/portal/portal.module.ts
@@ -5,13 +5,23 @@ import { PanelRoutingModule } from './portal-routing.module';
 import { UserComponent } from './page/user/user.component';
 import { DeviceComponent } from './page/device/device.component';
 import { BatteryComponent } from './page/battery/battery.component';
+import { DeviceTableComponent } from './components/device-table/device-table.component';
+import { BatteryTableComponent } from './components/battery-table/battery-table.component';
+import { EditDeviceComponent } from './components/edit-device/edit-device.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditBatteryComponent } from './components/edit-battery/edit-battery.component';
 
 @NgModule({
   declarations: [
     UserComponent,
     DeviceComponent,
-    BatteryComponent
+    BatteryComponent,
+    DeviceTableComponent,
+    BatteryTableComponent,
+    EditDeviceComponent,
+    EditBatteryComponent,
   ],
-  imports: [CommonModule, PanelRoutingModule],
+  imports: [CommonModule, PanelRoutingModule, CoreModule, ReactiveFormsModule],
 })
 export class PortalModule {}
