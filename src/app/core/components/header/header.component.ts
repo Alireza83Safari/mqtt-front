44d235cd9eaa74e33c10443b7ed4fb615f725eff
Alireza <alireza83safarii@gmail.com>
2024-08-18 23:38:13 +0330

    feat: add logout and profile

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { ProfileService } from '../../services/profile.service';
+import { Profile } from '../../models/profile';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  profileInfo!: Profile;
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private profileService: ProfileService
+  ) {}
+
+  ngOnInit(): void {
+    this.profileService
+      .getProfile()
+      .subscribe((res) => (this.profileInfo = res.result));
+  }
+
+  logout() {
+    this.authService.logout().subscribe((res) => {
+      if (res) {
+        this.toastrService.success('You have successfully logged out.');
+      }
+    });
+  }
+}
