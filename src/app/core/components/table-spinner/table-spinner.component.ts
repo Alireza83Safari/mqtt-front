20016811218fd5aff710d18d5beb14354505623b
Alireza <alireza83safarii@gmail.com>
2024-08-20 11:46:27 +0330

    feat: add table spinner on core module

--- src/app/core/components/table-spinner/table-spinner.component.ts
+++ src/app/core/components/table-spinner/table-spinner.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-table-spinner',
   templateUrl: './table-spinner.component.html',
-  styleUrls: ['./table-spinner.component.scss'],
 })
 export class TableSpinnerComponent {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { PanelLayoutComponent } from './layout/portal-layout/portal-layout.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ModalComponent } from './components/modal/modal.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { TableSpinnerComponent } from './components/table-spinner/table-spinner.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import { PaginationComponent } from './components/pagination/pagination.componen
     SidebarComponent,
     ModalComponent,
     PaginationComponent,
+    TableSpinnerComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
@@ -23,6 +24,7 @@ import { PaginationComponent } from './components/pagination/pagination.componen
     SidebarComponent,
     ModalComponent,
     PaginationComponent,
+    TableSpinnerComponent,
   ],
 })
 export class CoreModule {}
