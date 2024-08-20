30d9fe1924138839b542833209cd16b106f7c038
Alireza <alireza83safarii@gmail.com>
2024-08-20 11:40:12 +0330

    feat: add table spinner

---
+++ src/app/core/components/table-spinner/table-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-spinner',
+  templateUrl: './table-spinner.component.html',
+  styleUrls: ['./table-spinner.component.scss'],
+})
+export class TableSpinnerComponent {}
