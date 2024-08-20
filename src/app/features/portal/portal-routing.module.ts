8f848248055de39690191117501dbdfeacd9cdbb
Alireza <alireza83safarii@gmail.com>
2024-08-20 11:47:41 +0330

    feat: add new route on portal module

--- src/app/features/portal/portal-routing.module.ts
+++ src/app/features/portal/portal-routing.module.ts
@@ -4,11 +4,15 @@ import { UserComponent } from './page/user/user.component';
 import { PageRoutes } from 'src/app/core/routes/pageRoutes';
 import { BatteryComponent } from './page/battery/battery.component';
 import { DeviceComponent } from './page/device/device.component';
+import { BatteryDetailsComponent } from './page/battery-details/battery-details.component';
+import { DeviceDetailsComponent } from './page/device-details/device-details.component';
 
 const routes: Routes = [
   { path: PageRoutes.USER, component: UserComponent },
   { path: PageRoutes.BATTERY, component: BatteryComponent },
   { path: PageRoutes.DEVICE, component: DeviceComponent },
+  { path: PageRoutes.BATTERY_SENSOR, component: BatteryDetailsComponent },
+  { path: PageRoutes.DEVICE_METRIC, component: DeviceDetailsComponent },
 ];
 
 @NgModule({
--- src/app/features/portal/portal.module.ts
+++ src/app/features/portal/portal.module.ts
@@ -9,8 +9,17 @@ import { DeviceTableComponent } from './components/device-table/device-table.com
 import { BatteryTableComponent } from './components/battery-table/battery-table.component';
 import { EditDeviceComponent } from './components/edit-device/edit-device.component';
 import { CoreModule } from 'src/app/core/core.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditBatteryComponent } from './components/edit-battery/edit-battery.component';
+import { BatteryDetailsComponent } from './page/battery-details/battery-details.component';
+import { SensorChartComponent } from './components/sonsor-chart/sensor-chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { BatterySensorsComponent } from './components/battery-sensors/battery-sensors.component';
+import { DeviceMetricsComponent } from './components/device-metrics/device-metrics.component';
+import { DeviceDetailsComponent } from './page/device-details/device-details.component';
+import { MetricsChartComponent } from './components/metrics-chart/metrics-chart.component';
+import { MetricsChartContainerComponent } from './components/metrics-chart-container/metrics-chart-container.component';
+import { BatteryChartContainerComponent } from './components/battery-chart-container/battery-chart-container.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +30,23 @@ import { EditBatteryComponent } from './components/edit-battery/edit-battery.com
     BatteryTableComponent,
     EditDeviceComponent,
     EditBatteryComponent,
+    BatteryDetailsComponent,
+    BatteryChartContainerComponent,
+    SensorChartComponent,
+    BatterySensorsComponent,
+    DeviceMetricsComponent,
+    DeviceDetailsComponent,
+    MetricsChartComponent,
+    MetricsChartContainerComponent,
+    BatteryChartContainerComponent,
   ],
-  imports: [CommonModule, PanelRoutingModule, CoreModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    PanelRoutingModule,
+    CoreModule,
+    ReactiveFormsModule,
+    NgApexchartsModule,
+    FormsModule,
+  ],
 })
 export class PortalModule {}
